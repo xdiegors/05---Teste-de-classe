12c039039c127c9b0d5fa890bd77e2b8d2ede248
xdiegors <xdiegors@outlook.com>
2022-01-19 19:51:38 -0300

    Teste de uso de classe

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05___Abstracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa PrimeiroCandidato = new Pessoa();
+            
+            PrimeiroCandidato.Nome = "Diego";
+            PrimeiroCandidato.Idade = 30;
+            PrimeiroCandidato.Profissao = "Desenvolvedor .NET";
+            PrimeiroCandidato.Apresentacao();
+        }
+    }
+}
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+
+    public int Idade { get; set; }
+
+    public string Profissao { get; set; }
+
+    public void Apresentacao()
+    {
+        Console.WriteLine($"Olá pessoal! Meu nome é {Nome}, tenho {Idade} anos e sou um(a) {Profissao}");
+    }
+}
